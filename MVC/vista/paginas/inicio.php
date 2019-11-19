8112b7b8c84fd6f0840a856cf12f0c0e706abb45
VirutaSoft <virutasoft@gmail.com>
2019-11-19 00:21:13 -0500

    totally implemented MVC patron

---
+++ MVC/vista/paginas/inicio.php
@@ -0,0 +1,30 @@
+<!-- contenido -->
+
+            <h2>Tabla de usuarios</h2>
+            <p>Aquí puede visualizar los usuarios del sistema</p>
+            <table class="table table-dark table-hover">
+                <thead>
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                    </tr>
+                    <tr>
+                        <td>Mary</td>
+                        <td>Moe</td>
+                        <td>mary@example.com</td>
+                    </tr>
+                    <tr>
+                        <td>July</td>
+                        <td>Dooley</td>
+                        <td>july@example.com</td>
+                    </tr>
+                </tbody>
+            </table>
--- MVC/vista/plantilla.php
+++ MVC/vista/plantilla.php
@@ -61,36 +61,13 @@
     <!-- contenido -->
     <div class="container-fluid py-5">
         <div class="container">
-            <h2>Tabla de usuarios</h2>
-            <p>Aquí puede visualizar los usuarios del sistema</p>
-            <table class="table table-dark table-hover">
-                <thead>
-                    <tr>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                    </tr>
-                    <tr>
-                        <td>Mary</td>
-                        <td>Moe</td>
-                        <td>mary@example.com</td>
-                    </tr>
-                    <tr>
-                        <td>July</td>
-                        <td>Dooley</td>
-                        <td>july@example.com</td>
-                    </tr>
-                </tbody>
-            </table>
+        <?php
+            include "paginas/inicio.php";
+        ?>
         </div>
-    </div>
+    </div>    
+
+    
 
 </body>
 
