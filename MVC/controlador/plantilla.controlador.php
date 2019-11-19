c36e8a3e91e3c0102e3706ec61a9976a957d0717
VirutaSoft <virutasoft@gmail.com>
2019-11-18 23:41:57 -0500

    starting to edit and organize the files to MVC patron - Create the object to comunicate plantilla.php with index.php

---
+++ MVC/controlador/plantilla.controlador.php
@@ -0,0 +1,6 @@
+<?php
+class ControladorPlantilla{
+    public function ctrTraerPlantilla(){
+        include "vista/plantilla.php";
+    }
+}
---
+++ MVC/index.php
@@ -0,0 +1,8 @@
+<?php
+    require_once "controlador/plantilla.controlador.php";
+
+    $plantilla = new ControladorPlantilla();
+    $plantilla->ctrTraerPlantilla();
+    
+
+?>
